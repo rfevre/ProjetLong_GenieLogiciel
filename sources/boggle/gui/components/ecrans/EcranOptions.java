ec5202266133add5b97b00238365069a8a47876e
Mustelmass <m.el-massaoudi@hotmail.fr>
2015-12-13 21:19:23 +0100

    Bouton retour, terminer

--- sources/boggle/gui/components/ecrans/EcranOptions.java
+++ sources/boggle/gui/components/ecrans/EcranOptions.java
@@ -5,15 +5,17 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-import java.util.ArrayList;
+import java.awt.event.MouseEvent;
 import java.util.List;
 
 import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 import boggle.gui.components.elements.CustomButton;
+import boggle.gui.core.Game;
 
 public class EcranOptions extends Ecran {
 
@@ -27,7 +29,7 @@ public class EcranOptions extends Ecran {
 	private JComboBox tailleGrille;
 	
 	private JLabel dicoLabel, scoreLabel, nbToursLabel, tempsJeuLabel, tailleGrilleLabel;
-	private Button browse;
+	private Button btnBrowse, btnRetour, btnValider;
 	
 	
 	
@@ -60,7 +62,9 @@ public class EcranOptions extends Ecran {
 		this.tempsJeuLabel = new JLabel("<html><h2> Temps de jeu par joueur </html></h2>");
 		this.tailleGrilleLabel = new JLabel("<html><h2> Taille de la grille </html></h2>");
 		
-		this.browse = new Button(1, "BROWSE", SwingConstants.CENTER, 100,30);
+		this.btnBrowse = new Button(1, "BROWSE", SwingConstants.CENTER, 100,30);
+		this.btnRetour = new Button(2, "RETOUR", SwingConstants.CENTER, 120, 50);
+		this.btnValider = new Button(3, "VALIDER", SwingConstants.CENTER, 120, 50);
 		
 		init();
 	}
@@ -86,7 +90,7 @@ public class EcranOptions extends Ecran {
 		this.add(dictionnaire, gbc);
 		
 		gbc.gridx=2; //gbc.gridy=0;
-		this.add(browse, gbc);
+		this.add(btnBrowse, gbc);
 		
 		// Deuxième ligne
 		gbc.gridx=0; gbc.gridy = 1;
@@ -121,21 +125,65 @@ public class EcranOptions extends Ecran {
 		tailleGrille.setPreferredSize(new Dimension(150, 30));
 		this.add(tailleGrille, gbc);
 		
+		// Cinquième ligne
+		gbc.gridx =0; gbc.gridy=5;
+		gbc.insets = new Insets(100,0,10,100);
+		this.btnRetour.setBackground(Color.RED);
+		this.add(btnRetour, gbc);
+		
+		gbc.gridx = 5; //gbc.gridy=5;
+		gbc.insets = new Insets(100,100,10,100);
+		this.btnValider.setBackground(Color.RED);
+		this.add(btnValider, gbc);
+		
 	}
 	
 	/**
 	 * Classe interne Button
-	 * @author Mustapha
 	 *
 	 */
 	private class Button extends CustomButton{
 
 		private static final long serialVersionUID = 1L;
-
+		
+		/**
+		 * Constructeur de la classe intern bouton
+		 * @param id : correspond à l'id du boutton
+		 * @param libelle :  correspond à libelle du bouton
+		 * @param alignement : correspond à l'alignement du bouton
+		 * @param w : correspond à la largeur du bouton
+		 * @param h : correspond à la hauteur du bouton
+		 */
 		public Button(int id, String libelle, int alignement, int w, int h) {
 			super(id, libelle, alignement, w, h);
 			setBackground(Color.GRAY);
 		}
+		
+		@Override
+		/**
+		 * Classe qui correspond à l'évenement : 'pression sur la souris'
+		 */
+		public void mousePressed(MouseEvent e) {
+			
+			Button button = (Button)e.getSource();
+			if(button.getId() == 1) // Bouton browse
+			{
+				
+			}
+			else if(button.getId() == 2) // Bouton retour
+			{ 
+				Game.goToEcran(TypeEcrans.MENU);
+			}
+			else if(button.getId() == 3) // Bouton valider
+			{  
+				
+				// Modif à faire en fonction des choix de l'utilisateur
+			}
+				
+			
+		}
+		
+		
 	}
 	
 
